318acfede0cb8b2a144ee83cfbe8743abfc65e19
Ian Torres <iantorres@outlook.com>
2024-09-01 19:29:35 -0400

    Basic XML structure.

--- src/DTE.php
+++ src/DTE.php
@@ -6,19 +6,43 @@ class DTE
 {
     /**
      * Author
+     *
      * @const AUTHOR
      */
     private const array AUTHOR = [
-        "name" => "Ian Torres",
-        "email" => "iantorres@outlook.com",
+        'name' => 'Ian Torres',
+        'email' => 'iantorres@outlook.com',
     ];
+
+    public function __construct(public $three = new \DOMDocument('1.0', 'ISO-8859-1')) {}
 
     /**
      * Get the author
+     *
      * @return array|string[]
      */
     public static function getAuthor(): array
     {
         return static::AUTHOR;
+    }
+
+    /**
+     * Make an DTE
+     *
+     * @return DTE
+     */
+    public static function make(): DTE
+    {
+        return new static;
+    }
+
+    /**
+     * Serializes DTE
+     *
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->three->saveXML();
     }
 }
--- tests/Unit/DTETest.php
+++ tests/Unit/DTETest.php
@@ -2,7 +2,7 @@
 
 test('DTE version', function () {
     expect(\DTE\DTE::getAuthor())->toBe([
-        "name" => "Ian Torres",
-        "email" => "iantorres@outlook.com",
+        'name' => 'Ian Torres',
+        'email' => 'iantorres@outlook.com',
     ]);
 });
